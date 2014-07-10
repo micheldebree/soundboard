529c600d2f0830668230b6b109f888767710db9c
Michel de Bree <m.debree@gmail.com>
2014-07-10 09:25:14 +0200

    We got some sound!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,33 @@
  * Controller of the soundboardApp
  */
 angular.module('soundboardApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.sounds = [
+            {
+                id: "a1",
+                title: "Testje",
+                file: "sound1.mp3"
+            },
+            {
+                id: "a2",
+                title: "Test 2",
+                file: "sound2.mp3"
+            }];
+
+
+    })
+    .controller('BiteCtrl', ['$scope', function ($scope) {
+
+        $scope.audio = document.createElement('audio');
+        
+        $scope.init = function (sound) {
+            $scope.audio.id = sound.id;
+            $scope.audio.src = sound.file;
+        };
+        
+        $scope.play = function () {
+            $scope.audio.play();
+        };
+
+
+    }]);
